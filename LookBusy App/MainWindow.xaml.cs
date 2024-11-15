8afa04651c8174bc9e70ab49deda95b9f5d47da6
Dan <66948931+Danicus2000000@users.noreply.github.com>
2024-11-15 00:55:58 +0000

    Migrated look busy to .NET 9

--- LookBusy App/MainWindow.xaml.cs
+++ LookBusy App/MainWindow.xaml.cs
@@ -21,9 +21,9 @@ namespace LookBusy_App
     /// </summary>
     public partial class MainWindow : Window
     {
-        private System.Timers.Timer progressbarup = new System.Timers.Timer();
-        private System.Timers.Timer progressbardown = new System.Timers.Timer();
-        private Thread text;
+        private readonly System.Timers.Timer progressbarup = new();
+        private readonly System.Timers.Timer progressbardown = new();
+        private Thread? text;
         private bool threadinitalised = false;
         public MainWindow()
         {
@@ -53,7 +53,7 @@ namespace LookBusy_App
                 }
                 else
                 {
-                    Random rand = new Random();
+                    Random rand = new();
                     installprogress_pb.Value += rand.Next(10);
                 }
                 if (installprogress_pb.Value >= 50 && progressbardown.Enabled == false)
@@ -67,53 +67,52 @@ namespace LookBusy_App
             });
         }
 
-        private void showdetails_btn_Click(object sender, RoutedEventArgs e)//shows and hides text box
+        private void ShowDetailsBtnClick(object sender, RoutedEventArgs e)//shows and hides text box
         {
-            if (showdetails_btn.Content == "Show Details")
+            if ((string)showdetails_btn.Content == "Show Details")
             {
                 details_txt.Visibility = Visibility.Visible;
                 if (!threadinitalised)
                 {
-                    text = new Thread(() => dotext());
+                    text = new Thread(() => DoText());
                     text.Start();
                     threadinitalised = true;
                 }
                 showdetails_btn.Content = "Hide Details";
             }
-            else 
+            else
             {
                 details_txt.Visibility = Visibility.Hidden;
                 showdetails_btn.Content = "Show Details";
             }
         }
-        private void dotext() //generates random text for installer window
+        private void DoText() //generates random text for installer window
         {
-            string location = System.Reflection.Assembly.GetEntryAssembly().Location.Replace("LookBusy\\LookBusy App\\bin\\Debug\\net6.0-windows\\LookBusy App.dll", "");
-            string[] firstlocation = { "bin", "netcore", "main", "0978EAFC6J", "tomisgayontuesdays", "connorisaplankofwood", "canoflarger", "tomisatom" };
-            string[] secondlocation = { "debug", "longdancee", "legocity", "legolass", "gandalf", "danisachad", "012139123EASDJGSDIS" };
-            string[] thirdlocation = { "net6.0", "img", "css", "core", "lib", "repository", "git", "prettysusbrowhywouldyouevendothat", "71821EABAF" };
-            string[] forthlocation = { "vs_installer.msi", "index.html", "main.css", "console.sln", "pain.exe" };
-            string[] start = { "Extracting from", "Converting", "Refactoring", "Reset base", "Deleting" };
-            string[] end = { "from locale...", "index value revoked...", "repromanded...", "inversion intiated..." };
-            Random rand = new Random();
-            bool kill = false;
+            string location = System.Reflection.Assembly.GetEntryAssembly()?.Location.Replace("LookBusy\\LookBusy App\\bin\\Debug\\net6.0-windows\\LookBusy App.dll", "") ?? "";
+            string[] firstlocation = ["bin", "netcore", "main", "0978EAFC6J", "tomisgayontuesdays", "connorisaplankofwood", "canoflarger", "tomisatom"];
+            string[] secondlocation = ["debug", "longdancee", "legocity", "legolass", "gandalf", "danisachad", "012139123EASDJGSDIS"];
+            string[] thirdlocation = ["net6.0", "img", "css", "core", "lib", "repository", "git", "prettysusbrowhywouldyouevendothat", "71821EABAF"];
+            string[] forthlocation = ["vs_installer.msi", "index.html", "main.css", "console.sln", "pain.exe"];
+            string[] start = ["Extracting from", "Converting", "Refactoring", "Reset base", "Deleting"];
+            string[] end = ["from locale...", "index value revoked...", "repromanded...", "inversion intiated..."];
+            Random rand = new();
             while (true)
             {
                 this.Dispatcher.Invoke(() =>
                 {
                     details_txt.AppendText(start[rand.Next(start.Length)] + " " + location + firstlocation[rand.Next(firstlocation.Length)] + "\\" + secondlocation[rand.Next(secondlocation.Length)] + "\\" + thirdlocation[rand.Next(thirdlocation.Length)] + "\\" + forthlocation[rand.Next(forthlocation.Length)] + " " + end[rand.Next(end.Length)] + "\n");
                     details_txt.ScrollToEnd();
-                    if (details_txt.Text.Split("\n").Length >= 200) 
+                    if (details_txt.Text.Split("\n").Length >= 200)
                     {
-                        details_txt.Text = details_txt.GetLineText(details_txt.LineCount - 10) + details_txt.GetLineText(details_txt.LineCount - 9) + details_txt.GetLineText(details_txt.LineCount - 8) + details_txt.GetLineText(details_txt.LineCount - 7) + details_txt.GetLineText(details_txt.LineCount - 6) + details_txt.GetLineText(details_txt.LineCount - 5) + details_txt.GetLineText(details_txt.LineCount - 4) + details_txt.GetLineText(details_txt.LineCount - 3) + details_txt.GetLineText(details_txt.LineCount - 2) + details_txt.GetLineText(details_txt.LineCount-1);
+                        details_txt.Text = details_txt.GetLineText(details_txt.LineCount - 10) + details_txt.GetLineText(details_txt.LineCount - 9) + details_txt.GetLineText(details_txt.LineCount - 8) + details_txt.GetLineText(details_txt.LineCount - 7) + details_txt.GetLineText(details_txt.LineCount - 6) + details_txt.GetLineText(details_txt.LineCount - 5) + details_txt.GetLineText(details_txt.LineCount - 4) + details_txt.GetLineText(details_txt.LineCount - 3) + details_txt.GetLineText(details_txt.LineCount - 2) + details_txt.GetLineText(details_txt.LineCount - 1);
                     }
                 });
-                Thread.Sleep(rand.Next(40,80));
+                Thread.Sleep(rand.Next(40, 80));
             }
         }
-        private void cancel_btn_Click(object sender, RoutedEventArgs e)//if cancel pressed close window
+        private void CancelBtnClick(object sender, RoutedEventArgs e)//if cancel pressed close window
         {
-            MessageBoxResult result = MessageBox.Show("Would you like to canel this install, it is "+installprogress_pb.Value+"% completed, cancelling now may cause corruption.", "Warning", MessageBoxButton.YesNo);
+            MessageBoxResult result = MessageBox.Show("Would you like to canel this install, it is " + installprogress_pb.Value + "% completed, cancelling now may cause corruption.", "Warning", MessageBoxButton.YesNo);
             if (result == MessageBoxResult.Yes)
             {
                 Environment.Exit(Environment.ExitCode);
--- LookBusy/Program.cs
+++ LookBusy/Program.cs
@@ -3,16 +3,16 @@ Console.WriteLine("Welcome to look busy!");//randomises keywords and creates som
 Console.WriteLine("Press enter to begin looking busy");
 Console.ReadKey();
 Console.Clear();
-string location=System.Reflection.Assembly.GetEntryAssembly().Location.Replace("LookBusy\\LookBusy\\bin\\Debug\\net6.0\\LookBusy.dll","");
-string[] firstlocation = {"bin","netcore","main","0978EAFC6J","tomisgayontuesdays","connorisaplankofwood","canoflarger","tomisatom"};
-string[] secondlocation = {"debug","longdancee","legocity","legolass","gandalf","danisachad","012139123EASDJGSDIS"};
-string[] thirdlocation = {"net6.0","img","css","core","lib","repository","git","prettysusbrowhywouldyouevendothat","71821EABAF"};
-string[] forthlocation= {"vs_installer.msi","index.html","main.css","console.sln","pain.exe"};
-string[] start = { "Extracting from", "Converting","Refactoring","Reset base","Deleting" };
-string[] end = { "from locale...","index value revoked...","repromanded...","inversion intiated..." };
-Random rand = new Random();
-while (true) 
+string location = System.Reflection.Assembly.GetEntryAssembly()?.Location.Replace("LookBusy\\LookBusy\\bin\\Debug\\net6.0\\LookBusy.dll", "") ?? "";
+string[] firstlocation = ["bin", "netcore", "main", "0978EAFC6J", "tomisgayontuesdays", "connorisaplankofwood", "canoflarger", "tomisatom"];
+string[] secondlocation = ["debug", "longdancee", "legocity", "legolass", "gandalf", "danisachad", "012139123EASDJGSDIS"];
+string[] thirdlocation = ["net6.0", "img", "css", "core", "lib", "repository", "git", "prettysusbrowhywouldyouevendothat", "71821EABAF"];
+string[] forthlocation = ["vs_installer.msi", "index.html", "main.css", "console.sln", "pain.exe"];
+string[] start = ["Extracting from", "Converting", "Refactoring", "Reset base", "Deleting"];
+string[] end = ["from locale...", "index value revoked...", "repromanded...", "inversion intiated..."];
+Random rand = new();
+while (true)
 {
-    Console.WriteLine(start[rand.Next(start.Length)]+" " + location + firstlocation[rand.Next(firstlocation.Length)]+"\\" + secondlocation[rand.Next(secondlocation.Length)] + "\\" + thirdlocation[rand.Next(thirdlocation.Length)] + "\\" + forthlocation[rand.Next(forthlocation.Length)] +" " + end[rand.Next(end.Length)]);
+    Console.WriteLine(start[rand.Next(start.Length)] + " " + location + firstlocation[rand.Next(firstlocation.Length)] + "\\" + secondlocation[rand.Next(secondlocation.Length)] + "\\" + thirdlocation[rand.Next(thirdlocation.Length)] + "\\" + forthlocation[rand.Next(forthlocation.Length)] + " " + end[rand.Next(end.Length)]);
     Thread.Sleep(rand.Next(100, 500));
 }
